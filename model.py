8cbd53e236aef1273acf7a1e00ceff8834a34923
vy007vikas <wicked.yadav@gmail.com>
2017-11-05 11:53:23 +0530

    PtrNet model and forward code ready

--- model.py
+++ model.py
@@ -29,7 +29,7 @@ class PtrNet(nn.Module):
 		# for pointers
 		self.W_encoder = nn.Linear(self.hidden_dim, self.hidden_dim)
 		self.W_decoder = nn.Linear(self.hidden_dim, self.hidden_dim)
-		self.V = nn.Linear(self.hidden_dim, self.hidden_dim)
+		self.V = nn.Linear(self.hidden_dim, self.seq_len)
 
 
 	def forward(self, input):
@@ -43,27 +43,30 @@ class PtrNet(nn.Module):
 			encoded_input.append(c)
 
 		d_i = Variable(torch.Tensor([-1]*self.batch_size).view(self.batch_size, self.seq_len))			# B*I
+		distributions = []
 		for i in range(self.seq_len):
 			h, c = self.decoder[i](d_i, (h, c))				# B*H
 
 			# the attention part as obtained from the paper
 			# u_i[j] = v * tanh(W1 * e[j] + W2 * c_i)
-			c1 = self.W_decoder(c)							# B*H
+			u_i = []
+			c_i = self.W_decoder(c)								# B*H
 			for j in range(self.seq_len):
-				e1 = self.W_encoder(encoded_input[j])		# B*H
-				u = self.V(nn.tanh(c1 + e1))				# B*H
-				
+				e_j = self.W_encoder(encoded_input[j])			# B*H
+				u_j = self.V(F.tanh(c_i + e_j))					# B*N
+				u_i.append(u_j)
 
+			# a_i[j] = softmax(u_i[j])
+			u_i = torch.stack(u_i).t()			# B*N
+			a_i = F.softmax(u_i)				# B*N
+			distributions.append(a_i)
 
+			# d_i+1 = sum(a_i[j]*e[j]) over j
+			d_i = 0
+			for j in range(self.seq_len):
+				# select jth column of a_i
+				a_j = torch.index_select(a_i, 1, torch.LongTensor([j]))			# B,
+				a_j = torch.expand(self.batch_size, self.hidden_dim)			# B*H
+				d_i = d_i + (a_j*encoded_input[j])								# B*H
 
-
-
-
-
-
-
-
-
-
-
-
+		return distributions
